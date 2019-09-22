12259bd19a52ff6e16dd08d923dfa5a3119f1bb0
wsuzume <wspyy9584@gmail.com>
2019-09-22 20:11:45 +0900

    delete sudo

--- install_script/components/docker/install.sh
+++ install_script/components/docker/install.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 # Install Docker
-sudo yum remove docker docker-common docker-selinux docker-engine
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+yum remove docker docker-common docker-selinux docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum makecache fast
-sudo yum install docker-ce
+yum makecache fast
+yum install docker-ce
 
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-
-sudo docker run hello-world
+systemctl start docker.service
+systemctl enable docker.service
 
 # Install Docker Compose
-sudo yum install epel-release
-sudo yum install -y python-pip
-sudo pip install docker-compose
-sudo yum upgrade python*
+yum install epel-release
+yum install -y python-pip
+pip install docker-compose
+yum upgrade python*
 docker-compose version
