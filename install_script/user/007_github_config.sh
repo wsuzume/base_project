9d73d2f795c20615a52b654aaf393165262ab0a4
wsuzume <wspyy9584@gmail.com>
2019-09-22 19:37:14 +0900

    fix path

--- install_script/user/007_github_config.sh
+++ install_script/user/007_github_config.sh
@@ -2,5 +2,5 @@
 
 echo -e "Now it's time to register this server's public key to your GitHub account."
 echo -e "Execute\n"
-echo -e "$ scp username@address:/.ssh/id_rsa.pub destination\n"
+echo -e "$ scp username@address:~/.ssh/id_rsa.pub destination\n"
 echo -e "on your PC to download this server's public key."
