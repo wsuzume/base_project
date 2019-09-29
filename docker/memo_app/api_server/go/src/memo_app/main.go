1962c67faca991f89b25bb4f568eaa6ee33eef2e
Yohinobu Ogura <wspyy9584@gmail.com>
2019-09-30 02:29:51 +0900

    implement minimal response

--- docker/memo_app/api_server/go/src/memo_app/main.go
+++ docker/memo_app/api_server/go/src/memo_app/main.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	println("Hello, world!!")
-	return
+	router := gin.Default()
+
+	router.LoadHTMLGlob("views/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.GET("/signup", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "login.html", gin.H{})
+	})
+/*
+	router.POST("/login", func(ctx *gin.Context) {
+		println("post/login")
+	})
+*/
+	router.Run(":8080")
 }
+
